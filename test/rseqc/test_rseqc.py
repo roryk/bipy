1c0cd3cb2071d8703653bbe62bfa1ce072c9dc76
roryk <rory.kirchner@gmail.com>
2012-09-25 21:03:22 -0400

    Added a couple of more unit tests.

--- test/rseqc/test_rseqc.py
+++ test/rseqc/test_rseqc.py
@@ -31,6 +31,29 @@ class TestRseqc(unittest.TestCase):
         out_file = rseqc.clipping_profile(self.input_file, self.config)
         self.assertTrue(file_exists(out_file))
 
+    def test_genebody_coverage(self):
+        # test on the bam file
+        out_file_bam = rseqc.genebody_coverage(self.input_file, self.config)
+        print out_file_bam
+        self.assertTrue(file_exists(out_file_bam))
+        # test on the bigwig file
+        #bigwig = rseqc.bam2bigwig(self.input_file, self.config)
+        #out_file_bw = reseqc.genebody_coverage(bigwig, self.config)
+        #self.assertTrue(file_exists(out_file_bw))
+
+    def test_junction_annotation(self):
+        """ XXX this needs an organism with actual splice juntions """
+        out_file_junction = rseqc.junction_annotation(self.input_file,
+                                                      self.config)
+        print out_file_junction
+        self.assertTrue(file_exists(out_file_junction))
+
+    def test_junction_saturation(self):
+        """ XXX this needs an organism with actual splice junctions """
+        out_file_saturation = rseqc.junction_saturation(self.input_file,
+                                                        self.config)
+        print out_file_saturation
+        self.assertTrue(file_exists(out_file_saturation))
 
 if __name__ == "__main__":
     suite = unittest.TestLoader().loadTestsFromTestCase(TestRseqc)
