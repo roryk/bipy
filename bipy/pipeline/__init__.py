eaa6a52b2ff7a4169541db1757f26e5a93a3e9be
roryk <rory.kirchner@gmail.com>
2012-11-26 20:14:40 -0500

    Switched pipeline graphing over to __init__.py. Deleted parse.py.

--- bipy/pipeline/__init__.py
+++ bipy/pipeline/__init__.py
@@ -1,6 +1,8 @@
 """
 general functions for handling common pipeline steps
 """
+import networkx as nx
+import pylab
 from bipy.pipeline.stages import STAGE_LOOKUP
 from bipy.cluster import start_cluster, stop_cluster
 from bcbio.utils import safe_makedir, file_exists
@@ -145,3 +147,58 @@ class GenericPipeline(object):
         processed = self.view.map(stage, self.get_next_files)
         self.set_next_files(processed)
         return processed
+
+
+"""
+parse a configuration dict to set up the pipeline
+"""
+import networkx as nx
+import pylab
+import yaml
+import os
+
+class Node(object):
+
+    def __init__(self, name, block):
+        self.name = name
+        if block:
+            self.block = "block"
+        else:
+            self.block = "unblock"
+
+    def __str__(self):
+        return "%s - %s" % (self.name, self.block)
+
+
+class PipelineGraph(object):
+
+    def __init__(self, config):
+        self.config = config
+        #self.dag = pgv.AGraph(strict=True, directed=True)
+        self.dag = nx.DiGraph()
+        self.root = Node("start", True)
+        self.dag.add_node(self.root)
+        self._parse(config.get("run", {}), self.root)
+
+    def _parse(self, run, parent):
+        if parent not in self.dag:
+            self.dag.add_node(parent)
+        for item in run:
+            if isinstance(item, str):
+                child = Node(item, False)
+                self.dag.add_node(child)
+                self.dag.add_edge(parent, child)
+            elif isinstance(item, dict):
+                child = Node(item.keys()[0], True)
+                self.dag.add_node(child)
+                self.dag.add_edge(parent, child)
+                self._parse(item.values()[0], child)
+
+    def graph(self, out_file=None):
+        if out_file is None:
+            doc_dir = self.config["dir"].get("doc")
+            out_file = os.path.join(doc_dir, "pipeline_viz.png")
+        if file_exists(out_file):
+            return out_file
+        pd = nx.to_pydot(self.dag)
+        pd.write_png(out_file, prog="dot")
