5aca8b19a4976ae13ed8d2770802f5c7853c0d4a
roryk <rory.kirchner@gmail.com>
2012-11-30 19:31:01 -0500

    Added nice wrapper around cutadapt. Improved adapter + polyA trimming. Added unit test.

--- test/cutadapt/test_cutadapt.py
+++ test/cutadapt/test_cutadapt.py
@@ -1,12 +1,7 @@
-from itertools import repeat
-from bipy.log import setup_logging
-from bipy.utils import validate_config
-from bipy.log import logger
-from bipy.toolbox import cutadapt_tool
-from bcbio.utils import file_exists
-import sys
+from bipy.toolbox.trim import Cutadapt
 import yaml
 import unittest
+import filecmp
 
 CONFIG_FILE = "test/cutadapt/test_cutadapt.yaml"
 
@@ -16,11 +11,19 @@ class TestCutadapt(unittest.TestCase):
     def setUp(self):
         with open(CONFIG_FILE) as in_handle:
             self.config = yaml.load(in_handle)
-        self.input_files = self.config["input"]
+
         self.stage_config = self.config["stage"]["cutadapt"]
 
-    def test_run(self):
-        out_files = [cutadapt_tool.run(in_file, self.stage_config,
-                                       self.config) for
-                     in_file in self.input_files]
-        self.assertTrue = all(map(file_exists, out_files))
+    def test_single(self):
+        correct_file = self.config["input_single_correct"]
+        single = self.config["input_single"]
+        cutadapt = Cutadapt(self.config)
+        out_file = cutadapt(single)
+        self.assertTrue(filecmp.cmp(correct_file, out_file))
+
+    def test_pairedend(self):
+        correct_files = self.config["input_paired_correct"]
+        paired = self.config["input_paired"]
+        cutadapt = Cutadapt(self.config)
+        out_files = map(cutadapt, paired)
+        self.assertTrue(all(map(filecmp.cmp, correct_files, out_files)))
