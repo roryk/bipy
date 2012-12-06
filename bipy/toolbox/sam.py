c2016ae5317b0f01eae1878fa957a6777a7c9ab3
roryk <rory.kirchner@gmail.com>
2012-12-06 18:51:19 -0500

    Added disambiguation function plus unit test.

--- bipy/toolbox/sam.py
+++ bipy/toolbox/sam.py
@@ -4,13 +4,57 @@ wrappers around samtools
 import sh
 from bipy.utils import replace_suffix, append_stem, is_pair
 from bcbio.utils import file_exists
+from bcbio.distributed.transaction import file_transaction, _flatten_plus_safe
 import os
 import pysam
 from itertools import izip
+from bipy.pipeline.stages import AbstractStage
+
+
+def bam2sam(in_file, out_file=None):
+    """ convert a BAM file to a SAM file """
+    if is_sam(in_file):
+        return in_file
+
+    if out_file is None:
+        out_file = replace_suffix(in_file, "sam")
+
+    if file_exists(out_file):
+        return out_file
+
+    with file_transaction(out_file) as tmp_out_file:
+        sh.samtools.view(in_file, _out=tmp_out_file)
+
+    return out_file
+
+
+def is_sam_or_bam(in_file):
+    return is_bam(in_file) or is_sam(in_file)
+
+
+def is_bam(in_file):
+    _, ext = os.path.splitext(in_file)
+    if ext is ".bam":
+        return True
+    else:
+        return False
+
+
+def is_sam(in_file):
+    _, ext = os.path.splitext(in_file)
+    if ext is ".sam":
+        return True
+    else:
+        return False
 
 
 def sam2bam(in_file, out_file=None):
-    """ convert a SAM file to a BAM file """
+    """ convert a SAM file to a BAM file. if the file is already a
+    BAM file, return the BAM file name """
+
+    if is_bam(in_file):
+        return in_file
+
     if out_file is None:
         out_file = replace_suffix(in_file, "bam")
 
@@ -52,58 +96,121 @@ def bamdiff(pair, out_prefix=None):
 
     https://github.com/statgen/bamUtil
     """
+    out_dir = os.path.dirname(pair[0])
     if not out_prefix:
-        out_prefix = "diff.bam"
+        out_prefix = os.path.join(out_dir, "diff.bam")
+    else:
+        out_prefix = os.path.join(out_dir, out_prefix)
 
-    def get_out_file(out_prefix, in_file):
+    def get_out_file(out_prefix, in_file, number):
         base, _ = os.path.splitext(out_prefix)
-        in_base, in_ext = os.path.splitext(in_file)
-        out_file = "_".join([base, "only1", in_base]) + in_ext
+        in_base, in_ext = os.path.splitext(os.path.basename(in_file))
+        out_file = "_".join([base, "only" + str(number), in_base]) + in_ext
         return out_file
 
     if not is_pair(pair):
         raise ValueError("bamdiff needs to be run on a pair of input BAM "
                          "files.")
-    out_files = [get_out_file(out_prefix, x) for x in pair]
-    if all(file_exists(out_files)):
+    out_files = [get_out_file(out_prefix, y, x + 1)
+                 for x, y in enumerate(pair)]
+    if all(map(file_exists, out_files)):
         return out_files
 
-    sh.bam.diff(in1=pair[0], in2=pair[1], out=out_prefix)
+    sh.bam.diff("--in1", pair[0], "--in2", pair[1], "--out", out_prefix)
 
     return out_files
 
 
-def disambiguate(pair, cutoff=20):
+class Disambiguate(AbstractStage):
     """
-    takes two coordinate sorted BAM files and returns the reads unique
+    takes two read name sorted BAM files and returns the reads unique
     or poorly mapping to each file. cutoff specifies the difference in
     mapping quality to call a read as being specific in one file
 
     """
-    # first find the reads unique to file1 and file2
-    diff_pair = bamdiff(pair, "diff.bam")
 
-    # find the reads not unique between the two files
-    new_pairs = [(pair[0], diff_pair[0]), (pair[1], diff_pair[1])]
-    in_both = [bamdiff(x, out_prefix="contested.bam")[0] for x in new_pairs]
+    def __init__(self, config):
+        self.config = config
+        self.stage_config = config["stage"]["disambiguate"]
+        self.cutoff = self.stage_config.get("cutoff", 20)
 
-    # step through these contested reads and assign them to one file or
-    # the other based on passing the quality cutoff
-    assigned_files = _assign_by_quality(in_both, cutoff)
+    def __call__(self, pair):
+        unique_files = [append_stem(x, "unique") for x in pair]
+        ambig_files = [append_stem(x, "ambiguous") for x in pair]
+        if all(map(os.path.exists, unique_files + ambig_files)):
+            return [unique_files, ambig_files]
 
-    #XXX  merge the files together!
-    return assigned_files
+        handles_0 = self._get_handles(pair[0])
+        handles_1 = self._get_handles(pair[1])
+        self._process_reads(handles_0, handles_1, None, None)
+        [x.close() for x in handles_0]
+        [x.close() for x in handles_1]
+        return [unique_files, ambig_files]
+
+    def _get_handles(self, in_file):
+        assigned_name = append_stem(in_file, "unique")
+        ambiguous_name = append_stem(in_file, "ambiguous")
+
+        in_handle = pysam.Samfile(in_file, "rb")
+        assigned = pysam.Samfile(assigned_name, "wb", template=in_handle)
+        ambiguous = pysam.Samfile(ambiguous_name, "wb", template=in_handle)
+
+        return (in_handle, assigned, ambiguous)
+
+    def _dump_rest(self, handles, read):
+        (infile, assigned, ambiguous) = handles
+        if read:
+            assigned.write(read)
+        for read in infile:
+            assigned.write(read)
+
+    def _process_reads(self, handles_0, handles_1, read0, read1):
+        (in0, assigned0, ambiguous0) = handles_0
+        (in1, assigned1, ambiguous1) = handles_1
+        if read0 is None:
+            try:
+                read0 = in0.next()
+            except StopIteration:
+                self._dump_rest(handles_1, read1)
+                return True
+        if read1 is None:
+            try:
+                read1 = in1.next()
+            except StopIteration:
+                self._dump_rest(handles_0, read0)
+                return True
+
+        if read0.qname < read1.qname:
+            assigned0.write(read0)
+            read0 = None
+        elif read1.qname < read0.qname:
+            assigned1.write(read1)
+            read1 = None
+        else:
+            if (read0.mapq - read1.mapq) > 20:
+                assigned0.write(read0)
+            elif (read0.mapq - read1.mapq) < -20:
+                assigned1.write(read1)
+                read0 = None
+                read1 = None
+            else:
+                ambiguous0.write(read0)
+                ambiguous1.write(read1)
+            read0 = None
+            read1 = None
+
+        self._process_reads(handles_0, handles_1, read0, read1)
 
 
 def _assign_by_quality(pair, cutoff=20):
     in_handle1 = pysam.Samfile(pair[0], "rb")
     in_handle2 = pysam.Samfile(pair[1], "rb")
     out_files = [x.replace("contested", "assigned") for x in pair]
-    out_handle1 = pysam.Samfile(out_files[0], "wb"), template=in_handle1)
-    out_handle2 = pysam.Samfile(out_files[1], "wb"), template=in_handle2)
+    out_handle1 = pysam.Samfile(out_files[0], "wb", template=in_handle1)
+    out_handle2 = pysam.Samfile(out_files[1], "wb", template=in_handle2)
     ambig_files = [x.replace("contested", "ambiguous") for x in pair]
-    ambig_handle1 = pysam.Samfile(ambig_files[0], "wb"), template=in_handle1)
-    ambig_handle2 = pysam.Samfile(ambig_files[1], "wb"), template=in_handle2)
+    ambig_handle1 = pysam.Samfile(ambig_files[0], "wb", template=in_handle1)
+    ambig_handle2 = pysam.Samfile(ambig_files[1], "wb", template=in_handle2)
 
     for read1, read2 in izip(in_handle1.fetch(), in_handle2.fetch()):
         if (read1.mapq - read2.mapq) > cutoff:
---
+++ test/sam/test_sam.py
@@ -0,0 +1,42 @@
+import yaml
+from bipy.toolbox import sam
+from bipy.utils import flatten
+import unittest
+import os
+import hashlib
+
+cur_dir = os.path.dirname(os.path.realpath(__file__))
+CONFIG_FILE = os.path.join(cur_dir, "test_sam.yaml")
+
+
+class TestSam(unittest.TestCase):
+
+    def setUp(self):
+        with open(CONFIG_FILE) as in_handle:
+            self.config = yaml.load(in_handle)
+
+    def test_disambiguate(self):
+        in_files = self.config["input_bamdiff"]
+        disambiguate = sam.Disambiguate(self.config)
+        output = list(flatten(disambiguate(in_files)))
+        out_md5 = map(self._get_md5, output)
+        correct_files = self._correct_files(output)
+        correct_md5 = map(self._get_md5, correct_files)
+        self.assertTrue(out_md5 == correct_md5)
+        map(os.remove, output)
+
+    def _get_md5(self, out_file):
+        with open(out_file, "rb") as out_handle:
+            data = out_handle.read()
+        md5 = hashlib.md5(data)
+        return md5.digest()
+
+    def _correct_files(self, out_files):
+        dirs = map(os.path.dirname, out_files)
+        dirs = [os.path.join(x, "correct") for x in dirs]
+        basenames = map(os.path.basename, out_files)
+        return [os.path.join(d, f) for d, f in zip(dirs, basenames)]
+
+if __name__ == "__main__":
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestSam)
+    unittest.TextTestRunner(verbosity=2).run(suite)
