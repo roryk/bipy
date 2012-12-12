4ab6a6a4cb07366e9bb3ea90ddfa2245ee6e6943
roryk <rory.kirchner@gmail.com>
2012-12-12 18:49:04 -0500

    Added paired end unit test.

--- test/fastqc/test_fastqc.py
+++ test/fastqc/test_fastqc.py
@@ -14,17 +14,32 @@ class TestFastqc(unittest.TestCase):
     def setUp(self):
         with open(CONFIG_FILE) as in_handle:
             self.config = yaml.load(in_handle)
-        self.stage = fastqc.FastQCStage(self.config)
-        self.input = self.config["input"]
+        self.stage = fastqc.FastQC(self.config)
+
+    def _get_result(self, run_result):
+        return os.path.join(os.path.dirname(run_result),
+                            "test_fastqc_fastqc",
+                            "fastqc_data.txt")
 
     def test_fastqc(self):
+        input_single = self.config["input_single"]
         correct_file = os.path.join(cur_dir, "data", "correct_fastqc.txt")
-        run_result = self.stage(self.input)
+        run_result = self.stage(input_single)
         out_table = os.path.join(os.path.dirname(run_result),
                                  "test_fastqc_fastqc",
                                  "fastqc_data.txt")
         self.assertTrue(filecmp.cmp(correct_file, out_table))
         shutil.rmtree(os.path.join(cur_dir, "results"))
+
+    def test_fastqc_paired(self):
+        input_paired = self.config["input_paired"]
+        correct_file = [os.path.join(cur_dir, "data", "correct_fastqc.txt"),
+                        os.path.join(cur_dir, "data", "correct_fastqc.txt")]
+        run_result = self.stage(input_paired)
+        out_table = map(self._get_result, run_result)
+        self.assertTrue(all(map(filecmp.cmp, correct_file, out_table)))
+        shutil.rmtree(os.path.join(cur_dir, "results"))
+
 
 if __name__ == "__main__":
     suite = unittest.TestLoader().loadTestsFromTestCase(TestFastqc)
