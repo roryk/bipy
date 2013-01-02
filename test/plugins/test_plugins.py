057a1ce8008d0d3a3a2084e85511bf5c7793f9bb
roryk <rory.kirchner@gmail.com>
2013-01-01 19:23:24 -0500

    Added unit tests for finding the AbstractStage plugins.

---
+++ test/plugins/test_plugins.py
@@ -0,0 +1,58 @@
+import yaml
+import unittest
+from bipy.pipeline.stages import AbstractStage
+from bipy.plugins import StageRepository
+from bipy.toolbox.fastqc import FastQC
+from bcbio.utils import safe_makedir, file_exists
+import os
+
+STAGENAME = "plugins"
+
+
+class TestPlugins(unittest.TestCase):
+
+    def setUp(self):
+        self.config_file = os.path.join("test", STAGENAME,
+                                        "test_" + STAGENAME + ".yaml")
+        with open(self.config_file) as in_handle:
+            self.config = yaml.load(in_handle)
+        self.repository = StageRepository(self.config)
+
+    def test_repository(self):
+        """
+        test that the repository object gets made
+
+        """
+        repository = StageRepository(self.config)
+        self.assertTrue(isinstance(repository, StageRepository))
+
+    def test_builtin_plugins(self):
+        """
+        test that we can find some of the built in plugins
+
+        """
+        fastqc = self.repository["fastqc"]
+        self.assertTrue(fastqc is FastQC)
+
+    def test_custom_plugins(self):
+        """
+        test that the custom plugin directory is working
+
+        """
+        testplugin = self.repository["test_plugin"](self.config)
+        self.assertTrue(testplugin("Test") == "Test")
+
+
+class TestPlugin(AbstractStage):
+
+    stage = "test_plugin"
+
+    def __init__(self, config):
+        self.config = config
+
+    def __call__(self, string):
+        return string
+
+if __name__ == "__main__":
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestPlugins)
+    unittest.TextTestRunner(verbosity=2).run(suite)
