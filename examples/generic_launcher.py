c0e89a2706ae3810562a222e32e6cfd136326478
roryk <rory.kirchner@gmail.com>
2013-03-04 12:04:24 -0500

    Added support for running locally without using a scheduler.

--- examples/generic_launcher.py
+++ examples/generic_launcher.py
@@ -1,19 +1,27 @@
 import argparse
 from bipy.cluster import start_cluster, stop_cluster
 from bipy.log import setup_logging
+import sys
 
 def main():
     view.map(lambda x: "Hello World", xrange(10))
 
+def _args_valid_for_scheduler(args):
+    return args.scheduler and args.queue
+
+def _args_valid_for_local(args):
+    return not args.scheduler and not args.queue
+
 if __name__ == "__main__":
+
     parser = argparse.ArgumentParser(description='generic launcher')
     parser.add_argument('--profile', required=True,
                         help="IPython profile name to use")
     parser.add_argument('--cores', required=True,
                         help="Number of IPython engines to start.")
-    parser.add_argument('--queue', required=True,
+    parser.add_argument('--queue',
                         help="Name of queue to use.")
-    parser.add_argument('--scheduler', required=True,
+    parser.add_argument('--scheduler', default="",
                         help="Name of scheduler to use (LSF or SGE)")
     args = parser.parse_args()
 
@@ -23,7 +31,11 @@ if __name__ == "__main__":
                        "queue": args.queue,
                        "scheduler": args.scheduler}}
     setup_logging(cluster_config)
-    start_cluster(cluster_config)
-    from bipy.cluster import view
-    main()
-    stop_cluster()
+    if _args_valid_for_scheduler(args) or _args_valid_for_local(args):
+        start_cluster(cluster_config)
+        from bipy.cluster import view
+        main()
+        stop_cluster()
+    else:
+        print parser.usage()
+        sys.exit(1)
