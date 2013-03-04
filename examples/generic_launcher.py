849b9495f0b70357b2131799cb4e682574662ee7
roryk <rory.kirchner@gmail.com>
2013-03-04 11:31:39 -0500

    Added a script to launch a generic script from the command line.

---
+++ examples/generic_launcher.py
@@ -0,0 +1,28 @@
+import argparse
+from bipy.cluster import start_cluster, stop_cluster
+
+
+def main():
+    view.map(lambda x: "Hello World", xrange(10))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='generic launcher')
+    parser.add_argument('--profile', required=True,
+                        help="IPython profile name to use")
+    parser.add_argument('--cores', required=True,
+                        help="Number of IPython engines to start.")
+    parser.add_argument('--queue', required=True,
+                        help="Name of queue to use.")
+    parser.add_argument('--scheduler', required=True,
+                        help="Name of scheduler to use (LSF or SGE)")
+    args = parser.parse_args()
+
+    cluster_config = {"cluster":
+                      {"profile": args.profile,
+                       "cores": int(args.cores),
+                       "queue": args.queue,
+                       "scheduler": args.scheduler}}
+    start_cluster(cluster_config)
+    from bipy.cluster import view
+    main()
+    stop_cluster()
